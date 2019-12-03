74b8515e949d91d5d9c5ec7f09582605974b5011
him <you@example.com>
2019-12-03 21:55:28 +0200

    Updates

---
+++ precache-manifest.2d72828ac255f29b899f56a4bb3e77c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d88b9e9ede93fc22b347b4a46a7452",
+    "url": "/enger/index.html"
+  },
+  {
+    "revision": "73caa29452f4644ed249",
+    "url": "/enger/static/css/main.fb0cdfaa.chunk.css"
+  },
+  {
+    "revision": "30145a37e07b1e9450f0",
+    "url": "/enger/static/js/2.3e4584a4.chunk.js"
+  },
+  {
+    "revision": "73caa29452f4644ed249",
+    "url": "/enger/static/js/main.88758505.chunk.js"
+  },
+  {
+    "revision": "bffef60cbcd5cbe7f009",
+    "url": "/enger/static/js/runtime-main.f734a5c9.js"
+  }
+]);
